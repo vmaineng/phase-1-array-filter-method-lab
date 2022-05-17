940202871d649bff7b5a2f7994607d3bc3d400cc
Mai Vang <vmaineng@gmail.com>
2022-05-17 16:22:29 -0700

    commit tree 3c7af4364338d1fb4bdccb09a4433a6ba8bef56a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(drivers, string) {
+   return drivers.filter(function (name) {
+        return name.toLowerCase() === string.toLowerCase();
+      });
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (name) {
+         return name.splice(0) === string.splice(0);
+       });
+ }
+
+ function matchName(drivers, string) {
+    return drivers.filter(function (name) {
+         return name === string;
+       });
+ }
